efb0d63bab04cc39ef734351ddb5e8b523f775d6
nishat-sarker <nishatsarker90@gmail.com>
2022-03-31 01:38:02 +0600

    done for today

---
+++ 11_comparison_ope&booleans.js
@@ -0,0 +1,16 @@
+//booleans & comparison operator
+
+//booleans (true, false)
+let numberOne = 76;
+let numberTwo = "76";
+console.log(numberOne >= numberTwo);
+
+// == Vs ===
+console.log(numberOne == numberTwo); // == sudhu value check kore, dataType check kore na;
+
+console.log(numberOne === numberTwo); // === eti value and DataType dutai check kore;
+
+// != Vs !==
+console.log(numberOne != numberTwo); //only check regardless of operands type;
+
+console.log(numberOne !== numberTwo); // compare both value and type of 2 operands;
